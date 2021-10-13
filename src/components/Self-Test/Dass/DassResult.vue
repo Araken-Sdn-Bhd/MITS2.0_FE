4a1cb832b14005fd7a9f01a8c631e84031e7761e
Kisyok <kisoindran@gmail.com>
2021-10-13 16:03:50 +0800

    Updated DASS test

--- src/components/Self-Test/Dass/DassResult.vue
+++ src/components/Self-Test/Dass/DassResult.vue
@@ -1,39 +1,49 @@
 <template>
   <div>
+    <PortalHeader :testName="'Depression, Anxiety and Stress Scale (DASS)'"></PortalHeader>
 
-    <div class="container" id="title-print">
-        <div class="row" style="margin-bottom: 30px">
-            <div class="col-md">
-            </div>
-            <div class="col-md">
-                <h3>DIAGNOSIS</h3>
-            </div>
-            <div class="col-md">
-                <b-button @click="exportToPDF" size="sm" class="my-1"><b-icon icon="printer" aria-hidden="true"></b-icon> Print</b-button>
-            </div>
-        </div>
+    <div class="mt-5 row justify-content-center" id="print">
+      <div class="col-8 text-center">
+        <p class="englishSub">Thank you for reaching out to MENTARI Self Test.</p>
+      <p class="malaySub">Terima kasih kerana melayari ujian kendiri MENTARI.</p>
+      </div>
     </div>
+
+    <div class="row justify-content-center mt-4">
+      <div class="col-md-4 text-center">
+        <b-button id="print" @click="exportToPDF" size="sm"><b-icon icon="printer" aria-hidden="true"></b-icon> Print</b-button>
+      </div>
+    </div>
+
     <div ref="document">
-      <result :name="this.data.stress.name" :score="this.data.stress.score" :level="this.data.stress.level" :desc="this.data.stress.desc" :colour="this.data.stress.colour" :width="this.data.stress.width" :text="this.data.stress.text" :label="this.data.stress.label"></result>
-      <div class="break"></div>
-      <result :name="this.data.anxiety.name" :score="this.data.anxiety.score" :level="this.data.anxiety.level" :desc="this.data.anxiety.desc" :colour="this.data.anxiety.colour" :width="this.data.anxiety.width" :text="this.data.anxiety.text" :label="this.data.anxiety.label"></result>
-      <div class="break"></div>
-      <result :name="this.data.depression.name" :score="this.data.depression.score" :level="this.data.depression.level" :desc="this.data.depression.desc" :colour="this.data.depression.colour" :width="this.data.depression.width" :text="this.data.depression.text" :label="this.data.depression.label"></result>
+      <!-- <h3 id="print-header">DASS RESULTS</h3> -->
+      <result :name="this.data.stress.name" :nameBm="this.data.stress.nameBm" :score="this.data.stress.score" :level="this.data.stress.level" :levelBm="this.data.stress.levelBm" :desc="this.data.stress.desc" :descBm="this.data.stress.descBm" :colour="this.data.stress.colour" :width="this.data.stress.width" :label="this.data.stress.label" :labelBm="this.data.stress.labelBm"></result>
+      <result :name="this.data.anxiety.name" :nameBm="this.data.anxiety.nameBm" :score="this.data.anxiety.score" :level="this.data.anxiety.level" :levelBm="this.data.anxiety.levelBm" :desc="this.data.anxiety.desc" :descBm="this.data.anxiety.descBm" :colour="this.data.anxiety.colour" :width="this.data.anxiety.width" :label="this.data.anxiety.label" :labelBm="this.data.anxiety.labelBm"></result>
+      <result :name="this.data.depression.name" :nameBm="this.data.depression.nameBm" :score="this.data.depression.score" :level="this.data.depression.level" :levelBm="this.data.depression.levelBm" :desc="this.data.depression.desc" :descBm="this.data.depression.descBm" :colour="this.data.depression.colour" :width="this.data.depression.width" :label="this.data.depression.label" :labelBm="this.data.depression.labelBm"></result>
     </div>
 
     <div class="btnGroup">
       <button @click="redirectToDass" id = "returnBtn" type="button" class="btn">Return</button>
-      <button  @click="redirectToReqAppt" id = "reqBtn" type="button" class="btn">Request Appointment</button>
+      <!-- <button  @click="redirectToReqAppt" id = "reqBtn" type="button" class="btn">Request Appointment</button> -->
+    </div>
+
+    <div class="mt-5 row justify-content-center text-light bg-dark pt-3" id="print">
+      <div class="col-7 text-center">
+        <p class="disclaimer">*Please note, this short questionnaire is just a guide and the feelings you may be experiencing could be something other than stress, anxiety or depression.
+          (*Sila ambil maklum, ujian ringkas ini adalah sebagai petunjuk sahaja manakala perasaan yang anda sedang hadapi mungkin lebih berat.)</p>
+        <p class="disclaimer">*This is not a diagnosis. An accurate diagnosis for any mental health condition or disorder must be provided by a health professional.
+          (*Ini bukanlah diagnosis penyakit. Diagnosis yang tepat untuk sesuatu keadaan/ penyakit hendaklah diberikan oleh pegawai perubatan yang bertauliah.)</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import result from './dassResultChild'
-import html2pdf from 'html2pdf.js'
+import PortalHeader from '../PortalHeader'
 
 export default {
-  components: {result},
+  components: {result, PortalHeader},
   data(){
     return{
       data: {}
@@ -41,20 +51,13 @@ export default {
   },
   methods: {
 			exportToPDF () {
-				html2pdf(this.$refs.document, {
-					margin: [0.5, 0, 0.5, 0],
-					filename: 'DASS Test Result.pdf',
-					image: { type: 'jpeg', quality: 1.0 },
-					html2canvas: { dpi: 192, letterRendering: true, scale: 2, windowWidth: 400 },
-					jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' },
-          pageBreak: {mode: ['css', 'avoid-all'] }
-				})
+				window.print()
 			},
       redirectToReqAppt(){
         this.$router.push({path: '/reqApp', query: {type: 2, id: this.$route.query.id}})
       },
       redirectToDass(){
-        this.$router.push({path: '/dass'})
+        this.$router.back()
       }
 	},
   created(){
@@ -63,7 +66,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped media="screen">
 h2{
   text-align: center;
   margin-bottom: 10px;
@@ -85,9 +88,6 @@ h2{
 #reqBtn:hover {
   opacity: 0.6;
 }
-.break{
-  page-break-after: always;
-}
 #returnBtn{
     margin: 10px;
     background-color: #33b76e;
@@ -106,5 +106,67 @@ h2{
     color: white;
     font-size: 14px;
     font-weight: 400;
+}
+#print-header{
+  display: none;
+}
+.portal-header{
+  color: #595142;
+  font-family: "Roboto", Sans-serif;
+  font-size: 53px;
+  font-weight: 600;
+  text-align: center;
+  font-size: 4vw;
+  padding: 20px;
+  background-color: #F7F6F1;
+}
+.malaySub{
+  font-style: italic;
+  color: dimgrey;
+  font-size: 1vw;
+}
+.englishSub{
+  font-size:1.5vw;
+}
+.disclaimer{
+  font-size:1vw;
+  font-style: italic;
+}
+@media (max-width: 760px) {
+    .portal-header{
+      font-size: 8vw;
+    }
+    .disclaimer{
+      font-size: 1.5vw;
+    }
+}
+@media (max-width: 500px) {
+    .malaySub{
+      font-size: 2vw;
+    }
+    .englishSub{
+      font-size:3vw;
+    }
+    .disclaimer{
+      font-size: 2vw;
+    }
+}
+</style>
+
+<style scoped media="print">
+@page{
+  size: 18in 25in;
+}
+@media print{
+    .btnGroup{
+      display: none;
+    }
+    #print{
+      display: none;
+    }
+    #print-header{
+      display: block;
+      text-align: center;
+    }
 }
 </style>
