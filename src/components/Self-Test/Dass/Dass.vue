f20dc267b0dcfb5b86f42134de3096e0bd58c953
Kisyok <kisoindran@gmail.com>
2021-10-25 17:28:59 +0800

    update axios

--- src/components/Self-Test/Dass/Dass.vue
+++ src/components/Self-Test/Dass/Dass.vue
@@ -315,14 +315,14 @@ export default {
     redirectToHome(){
       this.$router.push({path: '/dass'})
     },
-    async redirectToResult(){
+    redirectToResult(){
       this.stressScore = this.model.stress.reduce((a, b) => a + b, 0)
       this.anxietyScore = this.model.anxiety.reduce((a, b) => a + b, 0)
       this.depressionScore = this.model.depression.reduce((a, b) => a + b, 0)
 
-      const url = 'http://127.0.0.1:8000/api/getTestRange?type=2'
-      const response = await this.$axios.get(url);
-      this.rangeInfo = response.data.data;
+      // const url = 'http://127.0.0.1:8000/api/getTestRange?type=2'
+      // const response = await this.$axios.get(url);
+      // this.rangeInfo = response.data.data;
 
       if(this.stressScore>=this.rangeInfo[0].range_min_value && this.stressScore<this.rangeInfo[0].range_max_value+1){
         this.stressLevel = this.rangeInfo[0].range_label
@@ -499,6 +499,13 @@ export default {
                this.$router.push({path: '/dass-results', query: {id: response.data.id}})
         })
     }
+  },
+  created(){
+    this.$axios.get("http://127.0.0.1:8000/api/getTestRange?type=2")
+    .then(response => this.rangeInfo = response.data.data)
+    .catch(error => {
+      console.error("There was an error!", error);
+    });
   }
 }
 </script>
